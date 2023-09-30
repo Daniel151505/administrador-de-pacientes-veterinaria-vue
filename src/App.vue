de68aa48e2bf3b666a238e71ccd864d4f61e1455
Daniel151505 <daniel150520011@gmail.com>
2023-09-29 23:38:58 -0500

    Creando el componente para el formulario

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
 import HeaderVue from "./components/Header.vue";
+import FormularioVue from "./components/Formulario.vue";
 </script>
 
 <template>
   <div class="container mx-auto mt-20">
     <HeaderVue />
+
+    <div class="mt-12 md:flex">
+      <FormularioVue />
+    </div>
   </div>
 </template>
